a56512c880182dd0adc266e04a4d0bf1f6b465fd
xujianbo <xujianbo@vocoor.com>
2019-07-10 09:27:23 +0800

    while True; Try except; ThreadingTcpServer

--- TcpServer.py
+++ TcpServer.py
@@ -15,26 +15,30 @@ import socketserver
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
-    num = 50
 
     def handle(self):
-        self.data = self.request.recv(1024).strip()
+        while True:  # while True, 一直保持在线
+            try:
 
-        print("{} wrote:".format(self.client_address[0]))
+                self.data = self.request.recv(1024).strip()
 
-        print(self.data)
+                print("{} wrote:".format(self.client_address[0]))
 
-        # just send back the same data, but upper-cased
+                print(self.data)
 
-        msg = '{2_0=02345;01523;04321}'  # now on off
-        self.request.sendall(msg.encode('utf-8'))
+                # just send back the same data, but upper-cased
+
+                msg = '{2_0=02345;01523;04321}'  # now on off
+                self.request.sendall(msg.encode('utf-8'))
+            except ConnectionAbortedError:
+                print("ConnectionAbortedError")
 
 
 if __name__ == "__main__":
     HOST, PORT = "0.0.0.0", 9999
     # Create the server, binding to 0.0.0.0 on port 9999
 
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+    server = socketserver.ThreadingTCPServer((HOST, PORT), MyTCPHandler)  # 使用多线程，可以同时处理多个tcp连接
 
     # Activate the server; this will keep running until you
 
